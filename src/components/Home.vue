b659b513ba951ed0a7c291078dd5289ec8943485
miaoerduo <zhaoyu3210@foxmail.com>
2019-09-27 00:58:56 +0800

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+<div id='home'>
+  <div v-for='(comic, index) in comicList' :key='index' :id="index">
+    <div>{{ comic }}</div>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      comicList: ['犬夜叉', '火影', '海贼王']
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
